116188d8850a07944cdec2298466a6b38cab3590
Raf Cevallos <rafael.cevallos81@gmail.com>
2019-10-26 14:51:53 -0500

    fixed additional conflicts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,13 @@
 import design from "./design.js"
 import footer from "./footer.js"
 import stateDropdown from "./stateDropdown.js";
-<<<<<<< HEAD
 import submitButton from "./button.js";
-=======
 
 // Comment later
 design.buildAndAppendbackgroundImage()
 design.buildAndAppendheader()
 
 footer.buildAndAppendFooter()
->>>>>>> master
 // import data from './dataManager.js';
 
 // comment later
