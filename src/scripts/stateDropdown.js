a24afde2382991d1e43d1df8896c477c7a026c25
Raf Cevallos <rafael.cevallos81@gmail.com>
2019-10-26 14:16:23 -0500

    created ky-map html

--- src/scripts/stateDropdown.js
+++ src/scripts/stateDropdown.js
@@ -4,10 +4,9 @@ import data from './dataManager.js';
 const stateDropdown = {
     dropdownContainer: document.querySelector("#state-dropdown"),
     createDropdown() {
-        this.dropdownContainer.innerHTML = `<select id="states"></select>`
+        this.dropdownContainer.innerHTML = `<select id="states"><option disabled selected>Select State</option></select>`
         const stateOptions = document.querySelector("#states")
         data.getStates().then(statesData => {
-            console.log(statesData)
             const statesArray = statesData.states
             statesArray.forEach(state => {
                 stateOptions.innerHTML += `<option value="${state.abbreviation}">${state.name}</option>`
