0107efce66f08481169a0ac466f7b92a7753367a
Raf Cevallos <rafael.cevallos81@gmail.com>
2019-10-26 14:47:45 -0500

    checking out the new master

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 import stateDropdown from "./stateDropdown.js";
+import submitButton from "./button.js";
 // import data from './dataManager.js';
 
 stateDropdown.createDropdown();
--- src/scripts/stateDropdown.js
+++ src/scripts/stateDropdown.js
@@ -4,7 +4,6 @@ import data from './dataManager.js';
 const stateDropdown = {
     dropdownContainer: document.querySelector("#state-dropdown"),
     createDropdown() {
-        this.dropdownContainer.innerHTML = `<select id="states"><option disabled selected>Select State</option></select>`
         const stateOptions = document.querySelector("#states")
         data.getStates().then(statesData => {
             const statesArray = statesData.states
@@ -12,6 +11,16 @@ const stateDropdown = {
                 stateOptions.innerHTML += `<option value="${state.abbreviation}">${state.name}</option>`
             })
         })
+    },
+    handleSubmitButton() {
+        /*
+        1. Whatever state is selected by user,
+        2. Get the correct state HTML file,
+        3. Create iFrame for the selected state's HTML file
+        */
+       const mapContainer = document.querySelector("#state-map"),
+             selectedState = event.target.value
+             console.log(selectedState)
     }
 }
 
