d7a5620590c285bc2add373aa041bf4cf830cd9e
Michelle Johnson <bob1234girl@gmail.com>
2019-10-27 11:46:03 -0500

    adding result fields

--- src/scripts/stateResults.js
+++ src/scripts/stateResults.js
@@ -1,10 +1,20 @@
 export default {
     buildAndAppendStateResults: () => {
         const stateResults = `
+            <article>
+                <h4 class=title>State Info</h4>
+                <div>
+                    <h5>Budget Amount><span>text text</span>
+                    <h5>Total Apllications</h5><span>text text</span>
+                    <h5>Total Green Ranking</h5><span>text text</span>
+                    <h5>Total Orange Ranking</h5><span>text text</span>
+                    <h5>Total Red Ranking</h5><span>text text</span>
+                </div>
+            </article>
     `
-        // console.log(stateResults)
+        console.log(stateResults)
 
-        const stateResultsContainerDiv = document.querySelector("#footer-container")
+        const stateResultsContainerDiv = document.querySelector("#stateResults")
         stateResultsContainerDiv.innerHTML = stateResults
     }
 }
