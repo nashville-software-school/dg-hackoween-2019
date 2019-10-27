cf8534f1bab8f7a7073be4e22b2ca3c3cd324a25
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-10-27 10:49:16 -0500

    populated application form based on application ID

--- src/scripts/dataManager.js
+++ src/scripts/dataManager.js
@@ -1,10 +1,16 @@
 // FIXME: change url to point to new and REAL JSON data
-const url = "https://raw.githubusercontent.com/nashville-software-school/dg-hackoween-2019/master/data/states.json"
+const stateURL = "https://raw.githubusercontent.com/nashville-software-school/dg-hackoween-2019/master/data/states.json"
 
+const applicationURL = "https://raw.githubusercontent.com/nashville-software-school/dg-hackoween-2019/master/data/apps_geocoded.json"
 
 const getStates = () => {
-    return fetch(`${url}`)
+    return fetch(`${stateURL}`)
     .then(response => response.json())
 }
 
-export default { getStates }
+const getApplicationData = () => {
+    return fetch(`${applicationURL}`)
+    .then(response => response.json())    
+}
+
+export default { getStates, getApplicationData }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,12 +2,22 @@ import design from "./design.js"
 import footer from "./footer.js"
 import stateDropdown from "./stateDropdown.js";
 import appForm from "./appForm.js";
+import dataManager from "./dataManager.js";
 
+
+dataManager.getApplicationData().then((data) => {
+    window.addEventListener("hashchange", event => {
+        const applicationId = parseInt(location.hash.split("#")[1])
+        const applicationsArray = data.applications
+        const selectedApplication = applicationsArray.find(application => application.id === applicationId)
+        appForm.buildAndAppendAppForm(selectedApplication)
+        
+    });
+})
 
 design.buildAndAppendbackgroundImage()
 design.buildAndAppendheader()
 
-appForm.buildAndAppendAppForm()
 
 footer.buildAndAppendFooter()
 
@@ -15,4 +25,3 @@ stateDropdown.createDropdown();
 
 document.querySelector("#state-dropdown").addEventListener("change", stateDropdown.handleSelection)
 
-window.addEventListener("hashchange", event => console.log(location.hash));
