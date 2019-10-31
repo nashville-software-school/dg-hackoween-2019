134c1b16f972db17601ae41a6fdc187cf399818d
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-10-31 14:51:48 -0500

    removed sample files

--- folium_example.py
+++
@@ -1,7 +0,0 @@
-import folium
-# folium 0.5.0
-
-m = folium.Map(location=[36.1627, -86.7816])
-folium.Marker([36.1627, -86.7816], popup='<a target="_blank" href = https://www.youtube.com/watch?v=oHg5SJYRHA0>Click me</a>').add_to(m);
-
-m.save('map.html')
